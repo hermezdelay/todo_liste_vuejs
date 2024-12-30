7022f0e0c9af75dc12cda1cb0da22415b6b84859
dalil16 <hermez.dalil@gmail.com>
2024-12-30 20:22:04 +0100

    installation de vue js

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-# Todo_liste_vuejs
+For the css i have used pico css
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vite.dev/config/).
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+>
 
 ## Project Setup
 
---
+++ src/views/Todo_liste.vue
@@ -0,0 +1,64 @@
+<script setup>
+</script>
+
+<template>
+    <div>This is your Todo liste</div>
+    <form action="" @submit.prevent="ajouter_element">
+      <fieldset role="group">
+        <input type="text" placeholder="tappez votre todo ici !" v-model="newtodo">
+        <button :disabled="newtodo.length == 0">Ajouter</button>
+      </fieldset>
+    </form>
+      <div v-if="todos.length === 0"> Vous n'avez pas de tàches à faire :( </div>
+      <div v-else>
+        <ul>
+          <li 
+          v-for="todo in todos"
+          :key="todo.date"
+          :class="{completed :  todo.completed}"
+          > 
+            <label for="">
+              <input type="checkbox" v-model="todo.completed">
+              {{ todo.title }}
+            </label>
+            </input>
+          </li>
+        </ul>
+      </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+//const todos = ref([])
+const todos = ref([ {
+  title : 'tache de test',
+  completed : true,
+  date : 1
+  }, {
+  title : 'tache à faire ',
+  completed : false,
+  date : 2
+  }
+])
+
+const newtodo = ref("")
+
+const ajouter_element = () => {
+  todos.value.push({
+    title : newtodo.value,
+    completed : false,
+    date : Date.now(),
+  })
+  newtodo.value=""
+}
+
+</script>
+
+<style>
+.completed {
+  opacity: 0.5;
+  text-decoration: line-through;
+}
+
+
+</style>
